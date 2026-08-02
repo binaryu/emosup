ebc6af8dd14d377b59e54cc9f5e6d0fa47c4d5be
binaryu <bin@wwzb.de>
2026-08-02 14:09:02 +0800

    chore: bump to v1.2.3

--- backend/internal/version/version.go
+++ backend/internal/version/version.go
@@ -2,7 +2,7 @@
 //
 // Set at build time via:
 //
-//	go build -ldflags "-X emosup/backend/internal/version.Version=v1.2.2"
+//	go build -ldflags "-X emosup/backend/internal/version.Version=v1.2.3"
 package version
 
 // Version is the emosup release version; "dev" for local builds.
